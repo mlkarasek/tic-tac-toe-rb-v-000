26f1f2183c983fcfb6f3e18275c1822d8de6721f
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 01:53:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,9 +89,9 @@ end
 end
 
 def draw?(board)
-  if !won?(board) && :full?(board)
+  if !won?(board) && !full?(board)
     true
-  elsif !won?(board) && :full?(board)
+  elsif !won?(board) && !full?(board)
     false
   else won?(board)
     false
