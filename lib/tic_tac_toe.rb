2fa7300325d9422a254fb1d79da0d2a416cf9b39
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 01:24:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,7 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
-def move(board, index)
+def move(board, index, player = "X")
   board[index] = player
   turn_count(board)
   display_board(board)
