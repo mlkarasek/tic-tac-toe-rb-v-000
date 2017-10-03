e54cf4d543fb850f1b3c6ea62c8652edf6e69994
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 02:01:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  while !over(board) && !won(board) && !draw?(board)
+  while !over(board) && !won?(board) && !draw?(board)
     turn(board)
 end
 if won?(board)
