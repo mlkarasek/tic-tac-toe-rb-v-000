4f0ac3be739458f34815b797c8b7335e95840342
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 01:45:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,6 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if !valid_move?(board, index)
-    move(board, index)
     turn(board)
   else
     move(board, index, current_player(board))
